4b71e4dff9dce792db5635166519ad14f303980f
ybb <34953126@qq.com>
2019-12-04 18:20:20 +0800

    优化

--- myioc/src/main/java/cn/haoxy/dao/IndexDaoImpl.java
+++ myioc/src/main/java/cn/haoxy/dao/IndexDaoImpl.java
@@ -2,12 +2,6 @@ package cn.haoxy.dao;
 
 import org.springframework.stereotype.Service;
 
-/**
- * @author Haoxy
- * Created in 2019-10-25.
- * E-mail:hxyHelloWorld@163.com
- * github:https://github.com/haoxiaoyong1014
- */
 
 @Service
 public class IndexDaoImpl implements IndexDao {
